042ef40df51351c105559a9c46d8f8358e9e8f07
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-01-10 18:34:43 +0530

    Seeder file

---
+++ seeder.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const connectDB = require("./config/config");
+const ItemModel = require("./models/itemModel");
+const items = require("./utils/data");
+
+//config
+dotenv.config();
+connectDB();
+
+// function seeder
+const importData = async () => {
+  try {
+    await ItemModel.deleteMany();
+    const itemsData = await ItemModel.insertMany(items);
+    console.log(`All Items added`);
+    process.exit();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+importData();
